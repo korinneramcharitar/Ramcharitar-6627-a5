cd7dd38d61170c9fe84156bcd8be1ccf075d762d
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-07-25 23:32:11 -0400

    Updates

--- src/test/java/ucf/assignments/InventoryManagerMenuControllerTest.java
+++ src/test/java/ucf/assignments/InventoryManagerMenuControllerTest.java
@@ -16,10 +16,11 @@ class InventoryManagerMenuControllerTest {
     @FXML
     public TextField EnterNameTextField;
 
-
+///there are some requirements that dont need a test such as the sorting inventory items
+    //this is because tableview has it's own sort function implemented
 
     @Test
-    void addItemButtonClicked() {
+    void check_items_added() {
         //push constants
 
 
@@ -28,15 +29,54 @@ class InventoryManagerMenuControllerTest {
         ;
         //if statement to make sure constant is being passed through
 
-                //true of false
+                // assert true if tableview has new data
 
             }
 
     @Test
-    void testAddItems() {
+    void check_items_deleted() {
+        //hard code items in tableview
 
+        //check that after item is deleted
 
+        //instance of deleted item is no longer detected
 
 
     }
+    @Test
+    void check_TSV_save(){
+        //check that filewriter function creates file with .txt extension
+    }
+    @Test
+    void check_HTML_save(){
+
+        //check that filewriter function creates file with .html extension
+    }
+    @Test
+    void check_JSON_save(){
+        //put values in tableview
+        //print values in filewriter
+        //check that filewriter function creates file with .json extension
+    }
+    @Test
+    void check_TSV_upload(){
+        //check that filereader pulls file that matches TSV format
+        //check data from file was imported to tableview
+    }
+    @Test
+    void check_HTML_format(){
+        //check that filereader pulls file that matches HTML format
+        //check data from file was imported to tableview
+    }
+    @Test
+    void check_JSON_format(){
+        //check that filereader pulls file that matches JSON format
+        //check data from file was imported to tableview
+    }
+    @Test
+    void check_Table_is_editable(){
+        //check table is editable
+    }
+
+
 }
---
+++ src/test/java/ucf/assignments/SearchItemsControllerTest.java
@@ -0,0 +1,17 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchItemsControllerTest {
+
+    @Test
+    void check_search_item(){
+    //check textfield recieves texts
+        //check that when textfield.getText equals itemID && itemName
+        //tableview data matches textfield.getText
+
+
+    }
+}
